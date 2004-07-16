2305a6d26429dca84df5a201f034874bbd88a3bc
csebold <csebold>
2004-07-16 20:25:55 +0000

    Fixed to allow only specified block tags, and started to allow CSS classes

--- textile.el
+++ textile.el
@@ -2,8 +2,7 @@
 ; $Id$
 
 (defun textile-code-to-blocks (start end)
-  (let ((my-region (buffer-substring-no-properties start end))
-        (this-block-type "p"))
+  (let ((my-region (buffer-substring-no-properties start end)))
     (with-temp-buffer
       (insert my-region)
       (beginning-of-buffer)
@@ -11,32 +10,46 @@
                          (save-excursion
                            (forward-paragraph 1)
                            (point))))
-        (narrow-to-region (point)
-                          (save-excursion
-                            (forward-paragraph 1)
-                            (if (looking-at "\n")
-                                (backward-char 1))
-                            (point)))
-        (beginning-of-buffer)
-        (if (looking-at "\n")
+        (let ((this-block-type "p")
+              (this-block-tag "p")
+              (this-block-class ""))
+          (narrow-to-region (point)
+                            (save-excursion
+                              (forward-paragraph 1)
+                              (if (looking-at "\n")
+                                  (backward-char 1))
+                              (point)))
+          (beginning-of-buffer)
+          (while (looking-at "\n")
             (forward-char 1))
-        (if (looking-at "\\([^ ]+\\)\\. ")
-            (progn
-              (setq this-block-type (match-string 1))
-              (delete-region (point)
-                             (save-excursion
-                               (re-search-forward "\\([^ ]+\\)\\. ")
-                               (point))))
-          (setq this-block-type "p"))
-        ; should determine what kind of block this is - FIXME
-        (beginning-of-buffer)
-        (if (looking-at "\n")
+          (if (looking-at "\\([^ ]+\\)\\. ")
+              (progn
+                (setq this-block-type (match-string 1))
+; should determine what kind of block this is - FIXME
+                (setq this-block-tag
+                      (cond
+                       ((string= this-block-type "bq") "blockquote")
+                       ((string-match "^h[1-6]$" this-block-type)
+                        this-block-type)
+                       ((string= this-block-type "fn")
+                        (progn
+                          (setq this-block-class "footnote")
+                          "p"))
+                       (t nil)))
+                (if this-block-tag
+                    (delete-region (point)
+                                   (save-excursion
+                                     (re-search-forward "\\([^ ]+\\)\\. ")
+                                     (point)))
+                  (setq this-block-tag "p"))))
+          (beginning-of-buffer)
+          (while (looking-at "\n")
             (forward-char 1))
-        (insert (concat "<" this-block-type ">"))
-        (end-of-buffer)
-        (insert (concat "</" this-block-type ">"))
-        (widen)
-        (forward-paragraph 1))
+          (insert (concat "<" this-block-tag ">"))
+          (end-of-buffer)
+          (insert (concat "</" this-block-tag ">"))
+          (widen)
+          (forward-paragraph 1)))
       (buffer-string))))
 
 (defun textile-region (start end)
