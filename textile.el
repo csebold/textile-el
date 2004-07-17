b1c348c8845ed0815f78f5207496a5f80eb90b6c
csebold <csebold>
2004-07-17 13:39:55 +0000

    Added comments on future directions; this code will have to be completely refactored at least once before primetime, the block handling code is terrible

--- textile.el
+++ textile.el
@@ -1,6 +1,26 @@
 ; textile.el
 ; $Id$
 
+; ideas for breaking this code out into subroutines: Instead of using
+; regular expressions to process everything, let's go through the
+; buffer and deal with things one block at a time, and in cases where
+; there are blocks within blocks, we can reprocess the buffer after
+; removing things like the bq. tag.  Then we can call a handler for
+; each different kind of block, and if the handler doesn't exist, then
+; we assume that it's supposed to be there in the actual text.
+
+; I need to figure out how to process
+; p{style}. p[lang]. p(class#id). and p> p< p= p<> and p( or p)
+; handlers.  Filters will probably have to wait for v2 of this code;
+; that looks even harder.
+
+; Is there a standard emacs way to process paragraphs?  Will it work
+; with traditional Textile code?
+
+; I think that Textile was really made for OOP and I'm not sure how
+; well Emacs will stand up to the task.  I don't mind requiring 'cl if
+; I have to but I really didn't want to have to use EIEIO for this.
+
 (defun textile-code-to-blocks (start end)
   (let ((my-region (buffer-substring-no-properties start end)))
     (with-temp-buffer
@@ -22,7 +42,8 @@
           (goto-char (point-min))
           (while (looking-at "\n")
             (forward-char 1))
-          (if (looking-at "\\([^ ]+\\)\\. ")
+          ; deal with extended blocks (Textile 2), FIXME
+          (if (looking-at "\\([^ .]+\\)\\. ")
               (progn
                 (setq this-block-type (match-string 1))
                 (setq this-block-tag
